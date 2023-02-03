10b3e151366f23e3a816b18e43b1ff291d4b0063
jenishraiyani <jenish.r@simformsolutions.com>
2023-02-03 16:42:24 +0530

    Added extra validation

--- app.js
+++ app.js
@@ -1,35 +1,58 @@
 let childScreen = document.getElementById("child-screen");
 let mainScreen = document.getElementById("output-screen");
 let errorMessage = document.getElementById("error-message");
+let operators = ["%", "+", "-", "*", "/", ".", "^", ".e+0"];
 let memoryItems = [];
 checkMemory();
 
 // Display user input on screen
 function displayEntry(value) {
   if (mainScreen.innerHTML == "0" && value != ".") {
-    mainScreen.innerHTML = value;
-  } else if (mainScreen.innerHTML.substr(mainScreen.innerHTML.length - 4) == ".e+0") {
-    mainScreen.innerHTML = mainScreen.innerHTML.slice(0, -1) + value;
-  } else {
-    if (ValidateInput(value)) {
+    if (operators.includes(value)) {
       mainScreen.innerHTML += value;
+    } else {
+      mainScreen.innerHTML = value;
     }
+  } else if (mainScreen.innerHTML.substr(mainScreen.innerHTML.length - 4) == ".e+0") {
+    if (operators.includes(value)) {
+      mainScreen.innerHTML = mainScreen.innerHTML;
+    } else {
+      mainScreen.innerHTML = mainScreen.innerHTML.slice(0, -1) + value;
+    }
+  }  
+    else {
+    if (ValidateInput(value) && mainScreen.innerHTML != "") {
+      mainScreen.innerHTML += value;
+    } else {
+      mainScreen.innerHTML = mainScreen.innerHTML.slice(0, -1) + value;
+    }
+  }
+}
+
+function displayOutput(value){
+  if(isNaN(value) || !isFinite(value)){
+    showError("Invalid Input");
+    mainScreen.innerHTML = "0";
+  }else{
+    mainScreen.innerHTML = value;
   }
 }
 
 // Calculate
 function Calculator() {
   try {
-    if (mainScreen.innerHTML != "") {
+    if (mainScreen.innerHTML != "" && !operators.includes(mainScreen.innerHTML.substr(mainScreen.innerHTML.length - 1))) {
       if (mainScreen.innerHTML.includes("^")) {
         const findXYSqaure = mainScreen.innerHTML.split("^", 2);
         childScreen.innerHTML = mainScreen.innerHTML;
-        mainScreen.innerHTML = Math.pow(findXYSqaure[0], findXYSqaure[1]);
+        displayOutput(Math.pow(findXYSqaure[0], findXYSqaure[1]));
       } else {
         childScreen.innerHTML = mainScreen.innerHTML;
         let count = eval(mainScreen.innerHTML);
-        mainScreen.innerHTML = count;
+        displayOutput(count);
       }
+    }else{
+      showError("Please enter valid input");
     }
   } catch (err) {
     showError("Please enter valid input");
@@ -38,16 +61,20 @@ function Calculator() {
 
 //Remove all the entry from screen
 function allClear() {
-  mainScreen.innerHTML = "";
+  mainScreen.innerHTML = "0";
   childScreen.innerHTML = "";
 }
 
 //Remove last entry
 function clearEntry() {
-  if (mainScreen.innerHTML == "") {
+  if (mainScreen.innerHTML == "0") {
     childScreen.innerHTML = childScreen.innerHTML.slice(0, -1);
   } else {
-    mainScreen.innerHTML = mainScreen.innerHTML.slice(0, -1);
+    if (mainScreen.innerHTML.length == 1) {
+      mainScreen.innerHTML = "0";
+    } else {
+      mainScreen.innerHTML = mainScreen.innerHTML.slice(0, -1);
+    }
   }
 }
 
@@ -72,15 +99,14 @@ function getConstant(clicked_id) {
 }
 
 function getDerivative() {
-  if(mainScreen.innerHTML == 0){
+  if (mainScreen.innerHTML == 0) {
     showError("Cannot divide by zero");
-  }else{
-    mainScreen.innerHTML = eval(1 / mainScreen.innerHTML);
+  } else {
+    displayOutput(eval(1 / mainScreen.innerHTML));
   }
- 
 }
 function getAbsoluteValue() {
-  mainScreen.innerHTML = Math.abs(mainScreen.innerHTML);
+  displayOutput(Math.abs(mainScreen.innerHTML));
 }
 
 function getModulo() {
@@ -99,32 +125,41 @@ function getFactorial() {
     for (var i = factorialNumber - 1; i >= 1; i--) {
       factorialNumber *= i;
     }
-    mainScreen.innerHTML = factorialNumber;
+    childScreen.innerHTML = `fact(${mainScreen.innerHTML})`;
+    displayOutput(factorialNumber);
   }
 }
 
 function getPower(clicked_id) {
   switch (clicked_id) {
     case "findSquare":
-      mainScreen.innerHTML = Math.pow(mainScreen.innerHTML, 2);
+      childScreen.innerHTML = `sqr(${mainScreen.innerHTML})`;
+      displayOutput(Math.pow(mainScreen.innerHTML, 2));
       break;
     case "findXRoot":
-      mainScreen.innerHTML = Math.pow(mainScreen.innerHTML, 1 / 2);
+      childScreen.innerHTML = `√(${mainScreen.innerHTML})`;
+      displayOutput(Math.pow(mainScreen.innerHTML, 1 / 2));
       break;
     case "findTenPower":
-      mainScreen.innerHTML = Math.pow(10, mainScreen.innerHTML);
+      childScreen.innerHTML = `10^(${mainScreen.innerHTML})`;
+      displayOutput(Math.pow(10, mainScreen.innerHTML));
       break;
     case "findXCube":
-      mainScreen.innerHTML = Math.pow(mainScreen.innerHTML, 3);
+      childScreen.innerHTML = `cube(${mainScreen.innerHTML})`;
+      displayOutput(Math.pow(mainScreen.innerHTML, 3));
       break;
     case "findCubeRoot":
-      mainScreen.innerHTML = Math.pow(mainScreen.innerHTML, 1 / 3);
+      childScreen.innerHTML = `cuberoot(${mainScreen.innerHTML})`;
+      displayOutput(Math.pow(mainScreen.innerHTML, 1 / 3));
       break;
     case "findTwoXSquare":
-      mainScreen.innerHTML = Math.pow(2, mainScreen.innerHTML);
+      childScreen.innerHTML = `2^(${mainScreen.innerHTML})`;
+      displayOutput(Math.pow(2, mainScreen.innerHTML));
       break;
     case "findEulerXSquare":
-      mainScreen.innerHTML = Math.pow(Math.E, mainScreen.innerHTML);
+      childScreen.innerHTML = `e^(${mainScreen.innerHTML})`;
+      displayOutput(Math.pow(Math.E, mainScreen.innerHTML));
+      break;
     case "findXYSqaure":
       if (ValidateInput("^")) {
         mainScreen.innerHTML += "^";
@@ -136,10 +171,12 @@ function getPower(clicked_id) {
 function getLog(clicked_id) {
   switch (clicked_id) {
     case "logTenBase":
-      mainScreen.innerHTML = Math.log10(mainScreen.innerHTML);
+        childScreen.innerHTML = `log(${mainScreen.innerHTML})`;
+        displayOutput(Math.log10(mainScreen.innerHTML));
       break;
     case "naturalLogarithm":
-      mainScreen.innerHTML = Math.log(mainScreen.innerHTML);
+      childScreen.innerHTML = `ln(${mainScreen.innerHTML})`;
+      displayOutput(Math.log(mainScreen.innerHTML));
       break;
   }
 }
@@ -147,17 +184,15 @@ function getLog(clicked_id) {
 function setPlusMinus() {
   let firstOperator = mainScreen.innerHTML.charAt(0);
   if (firstOperator == "-") {
-    getAbsoluteValue();
+    mainScreen.innerHTML = mainScreen.innerHTML.slice(1);
   } else {
-    mainScreen.innerHTML =
-      mainScreen.innerHTML.slice(0, 0) + "-" + mainScreen.innerHTML.slice(0);
+    mainScreen.innerHTML = mainScreen.innerHTML.slice(0, 0) + "-" + mainScreen.innerHTML.slice(0);
   }
 }
 
 //Validate user input
 function ValidateInput(value) {
   let lastEntry = mainScreen.innerHTML.slice(-1);
-  let operators = ["%", "+", "-", "*", "/", ".", "^", ".e+0"];
   if (operators.includes(value)) {
     if (operators.includes(lastEntry)) {
       return false;
@@ -195,16 +230,12 @@ function memoryPlusSubtract(clicked_id) {
       let lastItems = storedMemoryData.length - 1;
       switch (clicked_id) {
         case "memory-plus":
-          let replacePlus = eval(
-            storedMemoryData[lastItems] + "+" + mainScreen.innerHTML
-          );
+          let replacePlus = eval(storedMemoryData[lastItems] + "+" + mainScreen.innerHTML);
           storedMemoryData[lastItems] = replacePlus;
           localStorage.setItem("calcmemory", JSON.stringify(storedMemoryData));
           break;
         case "memory-subtract":
-          let replaceSubtract = eval(
-            storedMemoryData[lastItems] + "-" + mainScreen.innerHTML
-          );
+          let replaceSubtract = eval(storedMemoryData[lastItems] + "-" + mainScreen.innerHTML);
           storedMemoryData[lastItems] = replaceSubtract;
           localStorage.setItem("calcmemory", JSON.stringify(storedMemoryData));
           break;
@@ -242,13 +273,15 @@ function checkMemory() {
 function getMathFunctionValue(clicked_id) {
   switch (clicked_id) {
     case "rand":
-      mainScreen.innerHTML = Math.random();
+      displayOutput(Math.random());
       break;
     case "floor":
-      mainScreen.innerHTML = Math.floor(mainScreen.innerHTML);
+      childScreen.innerHTML = `floor(${mainScreen.innerHTML})`;
+      displayOutput(Math.floor(mainScreen.innerHTML));
       break;
     case "ceil":
-      mainScreen.innerHTML = Math.ceil(mainScreen.innerHTML);
+      childScreen.innerHTML = `ceil(${mainScreen.innerHTML})`;
+      displayOutput(Math.ceil(mainScreen.innerHTML));
       break;
   }
 }
@@ -256,29 +289,35 @@ function getMathFunctionValue(clicked_id) {
 function getTrigonometryValue(clicked_id) {
   switch (clicked_id) {
     case "sin":
-      mainScreen.innerHTML = Math.sin(mainScreen.innerHTML);
+      childScreen.innerHTML = `sin(${mainScreen.innerHTML})`;
+      displayOutput(Math.sin(mainScreen.innerHTML));
       break;
     case "cos":
-      mainScreen.innerHTML = Math.cos(mainScreen.innerHTML);
+      childScreen.innerHTML = `cos(${mainScreen.innerHTML})`;
+      displayOutput(Math.cos(mainScreen.innerHTML));
       break;
     case "tan":
-      mainScreen.innerHTML = Math.tan(mainScreen.innerHTML);
+      childScreen.innerHTML = `tan(${mainScreen.innerHTML})`;
+      displayOutput(Math.tan(mainScreen.innerHTML));
       break;
     case "sinh":
-      mainScreen.innerHTML = Math.sinh(mainScreen.innerHTML);
+      childScreen.innerHTML = `sinh(${mainScreen.innerHTML})`;
+      displayOutput(Math.sinh(mainScreen.innerHTML));
       break;
     case "cosh":
-      mainScreen.innerHTML = Math.cosh(mainScreen.innerHTML);
+      childScreen.innerHTML = `cosh(${mainScreen.innerHTML})`;
+      displayOutput(Math.cosh(mainScreen.innerHTML));
       break;
     case "tanh":
-      mainScreen.innerHTML = Math.tanh(mainScreen.innerHTML);
+      childScreen.innerHTML = `tanh(${mainScreen.innerHTML})`;
+      displayOutput(Math.tanh(mainScreen.innerHTML));
       break;
   }
 }
 
-function showError(msg){
+function showError(msg) {
   errorMessage.innerHTML = `<div class='alert alert-danger w-25' role='alert'>${msg}</div>`;
-    window.setTimeout(function () {
-      errorMessage.innerHTML = "";
-    }, 3000);
+  window.setTimeout(function () {
+    errorMessage.innerHTML = "";
+  }, 3000);
 }
